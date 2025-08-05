af52083d80cdb87763946bfb4066cac2faa9f9c0
Replicationpond <awls96@pm.me>
2025-08-05 19:16:19 -0400

    Add notification notify functionality

--- app/src/main/java/com/example/mainactivity/NotificationHelper.java
+++ app/src/main/java/com/example/mainactivity/NotificationHelper.java
@@ -1,8 +1,11 @@
 package com.example.mainactivity;
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -13,6 +16,7 @@ public class NotificationHelper {
         this.context = context.getApplicationContext();
     }
 
+    //Alerts the user when the CO2 value falls below a certain threshold, or did not meet an average breathing threshold.
     public void Alert_CO2(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -25,8 +29,19 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+            return;
+        }
     }
 
+    //Alerts the user when the temperature has noticeably spiked or when the main temperature is too high or low.
     public void Alert_Temperature(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -39,8 +54,19 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(2, builder.build());
+            return;
+        }
     }
 
+    //Alerts the user of an extreme spike in humidity or if it goes under or above a certain threshold.
     public void Alert_Humidity(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -53,8 +79,19 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(3, builder.build());
+            return;
+        }
     }
 
+    //Alerts the user that they have not fed their starter in 24 hours.
     public void Alert_Reminder(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -67,8 +104,19 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(4, builder.build());
+            return;
+        }
     }
 
+    //Alerts the user when the app decides the starter is ready to be fed.
     public void Alert_FeedingTime(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -81,8 +129,19 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(5, builder.build());
+            return;
+        }
     }
 
+    //Alerts the user that the starter has not gained any significant height in a calculated amount of time.
     public void Alert_Height(){
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -95,6 +154,16 @@ public class NotificationHelper {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(6, builder.build());
+            return;
+        }
     }
 
 }
